ca47e566cbe0f445ebb6265b1aa518ff8bc1f065
SuleymYovloy <evlo.suleym@mail.ru>
2020-02-14 19:38:34 +0300

    Сверстал, сделал адаптив

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
     svg4everybody({});
+
+    //кнопка бокового меню
+    const sandwichToggle = function () {
+        // Выбираем элементы, которые нам нужны. В примере мы ищем элементы с классом "sandwich"
+        let sandwichElements = document.querySelectorAll('.sandwich');
+        // Проходим циклом по всем эдементам и на каждый элемент вешаем слушателя, который по клику будет переключать класс
+        sandwichElements.forEach((item) => {
+            item.addEventListener('click', showSandwichTarget);
+            item.addEventListener('click', showSideMenu);
+        });
+    
+        function showSandwichTarget() {
+            let targetId = this.getAttribute('data-target-id'),
+                targetClassToggle = this.getAttribute('data-target-class-toggle');
+            this.classList.toggle('is-active');
+            if (targetId && targetClassToggle) {
+                document.getElementById(targetId).classList.toggle(targetClassToggle);
+            }
+        }
+
+        function showSideMenu() {
+            let menuNav = document.querySelector('.main-nav');
+            menuNav.classList.toggle('is-active');
+        }
+    };
+    sandwichToggle(); //вызов функции sandwichToggle
+    //
 });
